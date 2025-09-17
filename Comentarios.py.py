df07cf76c97b5acc5107bd8c8b4b66ad8c8275f0
Arambulakush57 <st5232@utr.edu.mx>
2025-09-17 07:36:03 -0600

    Comentarios

---
+++ Comentarios.py.py
@@ -0,0 +1,12 @@
+#Este programa imprime la variable. Tabmbien observamos la mutabilidad de python.
+
+entero = 10
+flotante = 3.14
+cadena = "Ed Flores"
+boleana = True
+
+print(entero)
+print(flotante)
+
+Christopher Javier MAcias Arambula
+ITPA
