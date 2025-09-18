2681608b5ea1294c2460345d04742d1b321f3292
Arambulakush57 <st5232@utr.edu.mx>
2025-09-18 07:53:53 -0600

    Lista1

---
+++ Lista1.py
@@ -0,0 +1,17 @@
+#vamos a declarar nuestras variables
+lista = ["Juan", "Ivan", "Adrian", "Jair"]
+print(lista)
+#imprimir lista
+print (lista[2])
+#sirve para agregar un item al final de la lista
+lista.append("Alexa")
+print (lista)
+lista.insert (1,"Javier")
+print (lista)
+#pop funciona para eliminar el ultimo elemento que se haya ingresado en la lista
+lista.pop()
+print (lista)
+lista.remove("Juan")
+print (lista)
+lista.pop(1)
+print(lista)
